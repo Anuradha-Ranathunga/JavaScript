11d91df2b3616ced2b28f6784a7b772baa8bc15d
Anuradha Ranathunga <anuradhaa2001@gmail.com>
2024-09-05 11:59:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// console.log("Hello World");
+// //This is comment
+
+// /* This is a multi line comment
+// This is 1st line
+// This is 2nd line
+// This is 3rd line */
+
+// var x =3;
+// console.log(x)
+
+// x= 5;
+// console.log(x)
+
+// x= x+1;
+// console.log(x)
+
+// x++;
+// console.log(x)
+
+// ++x;
+// console.log(x)
+
+// const c=3;
+// //c= 6;
+// console.log(c)
+
+// let d= 7;
+// console.log(d)
+
+// const e = 6;
+// console.log(e)
+
+var x = 5;
+let y = 8;
+let sum = x + y;
+console.log(sum)
+
+x= 7*6;
+console.log(x)
