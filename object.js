3d9d8507c2b14212993eb663b222612384a01ffb
Anuradha Ranathunga <Anuradhaa2001@gmail.com>
2024-10-12 11:57:02 +0530

    creata objects

---
+++ object.js
@@ -0,0 +1,4 @@
+let student = {firstName:"Saman" , lastName: "Gunasekara", Age: 50}
+student.firstName= "Pasindu";
+
+document.getElementById("x").innerHTML=" Hi, My name is " + student.firstName + " " + student.lastName+ ". I'm " +student.Age +" years old." ;
