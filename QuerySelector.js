2003002fe4ca691d5443a1771d3bf8c905b1ac5d
Anuradha Ranathunga <Anuradhaa2001@gmail.com>
2024-10-13 12:54:58 +0530

    Query Selector

---
+++ QuerySelector.js
@@ -0,0 +1,2 @@
+document.querySelector("p.test").style.backgroundColor="green";
+document.querySelector("#main").innerHTML="Hello, I'm Anuradha <br> Fruits <ul><li>Apple</li><li>Orange</li><li>Grapes</li></ul>";
